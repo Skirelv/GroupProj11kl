619bbd5d1a62d77bfd31b0f77bc112c0b309ffbb
Eriks Poikans <eriks@poikans.lv>
2025-03-12 16:47:22 +0200

    table insert

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import flask
-from flask import request, flash
 import pandas as pd
 from flask_peewee.db import Database
 import matplotlib.pyplot as plt
@@ -17,34 +16,30 @@ app.secret_key = 'dingledong'
 df = pd.read_csv('static/Example.csv', sep=';')
 #1st diagramm
 df.plot(x='Month', y='Amount', kind='bar', color='red')
-plt.title('Atzīmes pa nodarbībām')
+plt.title('Money spent')
 plt.legend().remove()
 plt.savefig('static/myplot1.png')
 #2nd diagramm
 labels=df['Month'].values
 df.plot.pie(y='Amount', labels=labels)
-plt.title('Atzīmes pa nodarbībām')
+plt.title('Money spent')
 plt.legend().remove()
 plt.savefig('static/myplot2.png')
+#csv show 
+table=df.to_html('templates/ExampTable.html')
 
 
 @app.route('/')
 def home():
     return flask.render_template('home.html')
 
-
 @app.route('/Upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-        if file.filename.endswith('.csv') == False:
-            flash('Not a csv file')
     return flask.render_template('upload.html')  
 
 @app.route('/example')
 def example():
+    
     return flask.render_template('example.html')  
 
 app.run()
