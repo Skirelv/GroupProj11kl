d933c0a73ec8211986122d766f2ef2af38e59582
Eriks Poikans <eriks@poikans.lv>
2025-03-13 23:10:45 +0200

    can upload to database files (yippeee)

--- main.py
+++ main.py
@@ -1,60 +1,68 @@
-import os
 import flask
 from flask import request
 import pandas as pd
 from flask_peewee.db import Database
+from peewee import FloatField, TextField 
 import matplotlib.pyplot as plt
 
-app = flask.Flask(__name__)
+import os
 
 DATABASE = {
-    'name': 'stats.db',
+    'name': 'data.db',
     'engine': 'peewee.SqliteDatabase'
 }
-app.secret_key = 'dingledong'
+SECRET_KEY = 'dingledong'
 
-#data
-df = pd.read_csv('static/Example.csv', sep=';')
-#1st diagramm
-df.plot(x='Month', y='Amount', kind='bar', color='red')
-plt.title('Money spent')
-plt.legend().remove()
-plt.savefig('static/myplot1.png', dpi=75)
-#2nd diagramm
-labels=df['Month'].values
-df.plot.pie(y='Amount', labels=labels)
-plt.ylabel('')
-plt.title('Money spent')
-plt.legend().remove()
-plt.savefig('static/myplot2.png', dpi=75)
-#csv show 
-table=df.to_html('templates/ExampTable.html')
+app = flask.Flask(__name__)
+app.config.from_object(__name__)
 
+db = Database(app)
+
+class Data(db.Model):
+    date = TextField()
+    billsum = FloatField()
 
 @app.route('/')
 def home():
     return flask.render_template('home.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST': 
         if 'file' not in request.files:
             return "No file part"
-        file = request.files['file']
-
-        if file.filename == '':
-            return "No selected file"
-
+        file = request.files['file'] 
         if file:
-            df = pd.read_csv(file)
-            print(df)
+            df = pd.read_csv(file, sep=';')
+            for index, row in df.iterrows():
+                Data.create(date=row['Month'], billsum=row['Amount'])
 
-            return f"CSV file uploaded successfully!<br><br>{df.to_html()}"
+            return flask.render_template('view.html')
     return flask.render_template('upload.html')  
+
 
 @app.route('/example')
 def example():
-    
+    #data
+    df = pd.read_csv('static/Example.csv', sep=';')
+    #1st diagramm
+    df.plot(x='Month', y='Amount', kind='bar', color='red')
+    plt.title('Money spent')
+    plt.legend().remove()
+    plt.savefig('static/myplot1.png', dpi=75)
+    #2nd diagramm
+    labels=df['Month'].values
+    df.plot.pie(y='Amount', labels=labels)
+    plt.ylabel('')
+    plt.title('Money spent')
+    plt.legend().remove()
+    plt.savefig('static/myplot2.png', dpi=75)
+    #csv show 
+    df.to_html('templates/examptable.html')
+
     return flask.render_template('example.html')  
 
-app.run()
+
+if __name__ == '__main__':
+    Data.create_table(fail_silently=True)
+    app.run(debug=True)
